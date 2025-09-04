632af1335633f29de86e32b787cc2070ee21ea0e
Jorge Menezes <116525199+Gmenezes3@users.noreply.github.com>
2025-09-03 22:55:13 -0300

    Upgrade 00.01
    - adição de arquivo index.html contendo a estrutura do formulário.
    - adição de arquivo style.css contendo uma pequena estilização.
    - adição de arquivo formulario.php contendo a logica.

---
+++ assets/formulario.php
@@ -0,0 +1,15 @@
+<?php
+
+$nota1 = ;
+$nota2 = ;
+$media = ($nota1 + $nota2)/2;
+
+if ($media >= 7){
+    echo "APROVADO. Sua média foi: $media";
+} 
+elseif ( $nota >= 4 || $nota <= 6.9 ) {
+    echo "Sua média final foi: $media. PROVA FINAL";
+    } 
+else
+    echo "Sua Média foi: $media. REPROVADO."
+?>
