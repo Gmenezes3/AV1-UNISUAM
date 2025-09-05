c27a2970af6572e2751a4c97814267e01e3a3d1a
Jorge Menezes <jorge_menezess@hotmail.com>
2025-09-04 23:54:27 -0300

    Update 00.04 - Refatorar tratamento do formulário e atualizar estilos
    feat: mover processamento para resultado.php, atualizar index.html, melhorar estilos e adicionar botão de retorno

---
+++ resultado.php
@@ -0,0 +1,21 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $matricula = $_POST["matricula"];
+    $nota1 = $_POST["nota1"];
+    $nota2 = $_POST["nota2"];
+};
+
+$media = ($nota1 + $nota2)/2;
+
+if ($media >= 7){
+    echo "$name, registrado na matricula Nº: $matricula.<br> Sua media final foi: $media. <strong>APROVADO.</strong>";
+} 
+elseif ( $media >= 4 && $media <= 6.9 ) {
+    echo "$name, registrado na matricula Nº: $matricula.<br> Sua media final foi: $media. <strong>RECUPERAÇÃO.</strong>";
+    } 
+else
+    echo "$name, registrado na matricula Nº: $matricula.<br> Sua media final foi: $media. <strong>REPROVADO.</strong><br>";
+
+echo'<br><br><button><a href="index.html">Voltar</a></button>'
+?>
